1af78e6ce6b06b433d87ba297119fa28aa2e894f
K. Shankari <shankari@eecs.berkeley.edu>
2020-03-20 00:19:07 -0700

    Force the version of conda to see if the environment works
    Otherwise, we have to install the conda version manually

--- setup/setup.sh
+++ setup/setup.sh
@@ -4,7 +4,8 @@ conda env create --name emsim --file setup/environment.yml
 # I can't get this to work in the environment file
 # maybe need to upgrade conda?
 pip install git+git://github.com/riccardoscalco/Pykov@master
-if [ ${CI} == "true" ] ; then
+
+if [ -z ${CI} ] && [[ ${CI} == "true" ]] ; then
     conda activate emsim
 else
     source activate emsim
