3fd6089a7d1eb002423ab631fbe56cebaa238fe5
K. Shankari <shankari@eecs.berkeley.edu>
2020-05-12 14:42:58 -0700

    Fixes while testing against a real OTP server
    + ‼️  convert the lat/lon to float before passing them in ‼️
    + ‼️  zip the arrays properly to create pairs between adjacent places not the same place ‼️
    
    + Make the filenames end in `.timeline`
    + remove the passed in `user_id` elsewhere and use `dummy_id` internally
    + return the entire JSON object for PathNotFound, not only the debug
    + make the output file writeable
    + remove objectids before sending the data to the server

--- emission/simulation/output.py
+++ emission/simulation/output.py
@@ -1,7 +1,10 @@
 import json
 
+import emission.simulation.client as esc
+
 def sync_to_file(measurement_list, fp):
-    json.dump(measurement_list, fp, indent=2)
+    measurements_no_id = [esc.EmissionFakeDataGenerator._remove_id_field(entry) for entry in measurement_list]
+    json.dump(measurements_no_id, fp, indent=2)
 
 def sync_to_server(measurement_list, client):
     pass
